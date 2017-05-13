3a104f4f667533fde1ba3c24c1590d7b13d5fd29
Thomas Weng <thomas.weng11@gmail.com>
2017-05-13 15:42:19 -0700

    Added mock data Added header component

--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+let data = {
+  "group_name": "Comic Book Club",
+  "users": 
+  {
+    "0": "Chihiro Isozaki",
+    "1": "Thomas Weng"
+  },
+  "posts": [
+    {
+      "title": "Hello World",
+      "url": "https://www.google.com",
+      "comments": [
+        {
+          "user": "0",
+          "body": "this is cool"
+        }
+      ]
+    }
+  ]
+};
+
+class Header extends React.Component {
+  render() {
+    const { headerProps } = this.props;
+    const { title, numUsers } = headerProps;
+
+    return <div id="header">
+      <div>{title}</div>
+      <div>{numUsers} members</div>
+    </div> 
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>
+    const { data } = this.props;
+
+    const headerProps = {
+      title: data.group_name,
+      numUsers: Object.keys(data.users).length
+    }
+
+    return <div>
+      <Header headerProps={headerProps} />
+    </div>
   }
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App data={ data }/>, document.getElementById('app'));
