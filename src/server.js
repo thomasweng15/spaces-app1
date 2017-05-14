cbb37b77a6c755cb9853f7fd99b73b2d053f9b5d
Thomas Weng <thomas.weng11@gmail.com>
2017-05-14 04:36:30 -0700

    rename public folder to static

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, '../index.html')
-    const publicPath = express.static(path.join(__dirname, '../public'))
+    const staticPath = express.static(path.join(__dirname, '../static'))
 
-    app.use('/public', publicPath)
+    app.use('/static', staticPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
