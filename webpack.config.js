aed3c301bcc92e22fd632d42c60c1f7c21d2ff44
Thomas Weng <thomas.weng11@gmail.com>
2017-05-14 02:34:37 -0700

    Change loader syntax for azure

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
         rules: [
           {
             test: /\.scss$/,
-            use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            use: [ 
+              { loader: 'style-loader' },
+              { loader: 'css-loader' }, 
+              { loader: 'sass-loader' }
+            ]
           }
         ]
       }
