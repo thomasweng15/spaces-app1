a323d33d4b01a3dcb5266f931d3fc95c05e3b3e8
Thomas Weng <thomas.weng11@gmail.com>
2017-05-14 00:05:03 -0700

    Separate prod and dev webpack configs

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'cheap-module-source-map',
   entry: './src/main.js',
   output: { 
     path: path.join(__dirname, 'public'), 
